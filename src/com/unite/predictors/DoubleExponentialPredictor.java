acdb416edd56fd31375e9825a4b7e12f448c96be
Michael <michael.loukeris@icloud.com>
2017-03-08 13:48:19 +0200

    Refactoring DoubleExponential Predictor

--- src/com/unite/predictors/DoubleExponentialPredictor.java
+++ src/com/unite/predictors/DoubleExponentialPredictor.java
@@ -1,31 +1,28 @@
 package com.unite.predictors;
 
-public class DoubleExponentialPredictor implements Predictor{
+public class DoubleExponentialPredictor extends AbstractPredictor{
 	//TODO What is this PARAMETER
-	private static final double PARAMETER = 0.6;
-	private double[] data;
+//	private static final double PARAMETER = 0.6;
+	private double parameter;
 	
-	
-	public void initialize(double[] a) {
-		data=a;
+	DoubleExponentialPredictor(double[] data, double parameter){
+		super(data);
+		this.parameter = parameter;
 	}
-
+	
 	public double getResult() {
-		int length;
-		double[] forecasts_table1,forecasts_table2;
-  
-		forecasts_table1=exponential(data, PARAMETER);
-		forecasts_table2=exponential(forecasts_table1, PARAMETER);
-		length=forecasts_table2.length;	
-		return forecasts_table2[length-1];
+		double[] forecasts_table;
+		
+		forecasts_table=exponential(data, parameter);
+		forecasts_table=exponential(forecasts_table, parameter);
+		return forecasts_table[forecasts_table.length-1];
 	}
 
-  	private static double[] exponential(double[] data, double a) {
-  		int i;
-  		double[] forecasts=new double[data.length+1];
-  		forecasts[0]=data[0];
-  		for(i=1;i<=data.length;i++) {
-  			forecasts[i]=a*data[i-1]+(1-a)*forecasts[i-1];
+  	private static double[] exponential(double[] input, double param) {
+  		double[] forecasts = new double[input.length+1];
+  		forecasts[0] = input[0];
+  		for(int i=1;i<=input.length;i++) {
+  			forecasts[i] = param * input[i-1] + (1-param) * forecasts[i-1];
   		}
 		  return forecasts;
 	  }
