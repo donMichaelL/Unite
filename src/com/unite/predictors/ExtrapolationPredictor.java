91274798083fdb080726b065eed4e303adb57593
Michael <michael.loukeris@icloud.com>
2017-03-08 14:26:17 +0200

    Refactoring Extrapolation Predictor

--- src/com/unite/predictors/ExtrapolationPredictor.java
+++ src/com/unite/predictors/ExtrapolationPredictor.java
@@ -1,16 +1,14 @@
 package com.unite.predictors;
 
-public class ExtrapolationPredictor implements Predictor{
-	private double[] data;
-	
-	public void initialize(double[] a) {
-		data = a;
-	}
-	
+public class ExtrapolationPredictor extends AbstractPredictor{
+	private static int MINIMUM_TABLE_SIZE = 2;
+
+	/**
+	 * throws NullPointerException
+	 */
 	public double getResult() {
-		if (data.length >= 2) 
-			return (2.0 * data[data.length - 1] - data[data.length - 2]);
-		else
-			return data[data.length - 1];
+		if (size() < MINIMUM_TABLE_SIZE) 
+			return data[size() - 1];
+		return (2.0 * data[size()-1] - data[size()-2]);
 	}
 }
