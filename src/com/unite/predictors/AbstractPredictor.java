f83a71a719628a14484918567c68652be5c109c1
Michael <michael.loukeris@icloud.com>
2017-03-08 11:26:50 +0200

    Adding Predictor and Abstract Predictor

---
+++ src/com/unite/predictors/AbstractPredictor.java
@@ -0,0 +1,19 @@
+package com.unite.predictors;
+
+public abstract class AbstractPredictor implements Predictor {
+	protected double[] data;
+	
+	public AbstractPredictor(double[] data) {
+		super();
+		initialize(data);
+	}
+	
+	public double[] getData() {
+		return data;
+	}
+
+	public void initialize(double[] a) {
+		data = a;
+	}
+	
+}
--- src/com/unite/predictors/Predictor.java
+++ src/com/unite/predictors/Predictor.java
@@ -1,6 +1,7 @@
 package com.unite.predictors;
 
 public interface Predictor {
+	
 	void initialize(double[] a);
 	
 	double getResult();
