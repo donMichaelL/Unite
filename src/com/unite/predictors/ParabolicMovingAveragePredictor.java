7dbd1615f243ffa14da7fe3fc071360eef0c4c3e
Michael <michael.loukeris@icloud.com>
2017-03-08 15:55:16 +0200

    ParabolicMovingAveragepredictor refactoring

--- src/com/unite/predictors/ParabolicMovingAveragePredictor.java
+++ src/com/unite/predictors/ParabolicMovingAveragePredictor.java
@@ -1,40 +1,35 @@
 package com.unite.predictors;
 
-public class ParabolicMovingAveragePredictor implements Predictor {
-	private double[] data;
-		
-	    
-	public void initialize(double[] a) {
-    	data=a;
+public class ParabolicMovingAveragePredictor extends AbstractPredictor {
+	private double trend;
+	
+	ParabolicMovingAveragePredictor(double[] data, double trend) {
+		super(data);
+		this.trend = trend;
 	}
-
+	    
 	public double getResult() {
-		// TODO IS THIS CONSTANT
-		if (data==null) return 0;
-		double trend=0.02;
-		double forecast;
-		forecast=exponential(data,trend);
-		return forecast;
+		return exponential(data, trend);
 	}
 
 	private static double exponential(double[] data, double trend) {
-		 int i;
 		 double[] sar=new double[data.length+1];
 		 sar[0]=data[0];
-		 double high,low;
-		 high=data[0];
-		 low=data[0];
+		 double high=data[0];
+		 double low=data[0];
 		
-		 for(i=1;i<=data.length;i++) {
-			 if(data[i-1]>high) {
-				 sar[i]=data[i-1]+trend*Math.abs(high-data[i-1]);
-				 high=data[i-1];
-				 trend=trend+0.02;
+		 for(int i=1;i<=data.length;i++) {
+			 if(data[i-1] > high) {
+				 sar[i] = data[i-1] + trend*Math.abs(high-data[i-1]);
+				 high = data[i-1];
+				 // TODO what is 0.02
+				 trend= trend+ 0.02;
 			 } else if (data[i-1]<high && data[i-1]>low ) {
 				 sar[i]=data[i-1]+trend*Math.abs(high-data[i-1]);
 			 } else {
 				 sar[i]=data[i-1]+trend*Math.abs(low-data[i-1]);
 				 low=data[i-1];
+				 // TODO what is 0.02
 				 trend=trend+0.02;
 			 }
 		 }
