d19b82f74f67702b237614ad49715980ae849d0f
Michael <michael.loukeris@icloud.com>
2017-03-10 11:39:16 +0200

    Creating static function with Generic Array argument

--- src/com/unite/predictors/MovingAveragePredictor.java
+++ src/com/unite/predictors/MovingAveragePredictor.java
@@ -11,21 +11,31 @@ public class MovingAveragePredictor extends AbstractPredictor {
     	else {this.period = period;}
 	}
 
+
+    public double getResult() {
+    	Integer[] dataObject = new Integer[size()];
+    	for (int i=0; i<size(); i++){
+    		dataObject[i]= (int) data[i];
+    	}
+    	return MovingAveragePredictor.calculateAlgorithm(dataObject, period);
+    }
+    
     /**
      * Iterating to the last period elements of the table
      * @return double moving average predictor
      * throws NullPointerException
      * throws IllegalArgumentException
      */
-    public double getResult() {
-    	//TODO negative check second if
-    	if (period==0) throw new IllegalArgumentException();
-    	if (size()==1) return data[size()-1];
+    public static <E extends Number> double calculateAlgorithm(E[] input, int period){
+    	if (period<=0) throw new IllegalArgumentException();
+    	int size = input.length;
+    	if (size==1) return input[size-1].doubleValue();
     	
-    	double total=0;
-    	for(int i=(size()-1);i>(size()-period-1);--i){
-    		total += data[i];
+    	double total= new Double(0);
+    	for(int i=(size-1);i>(size-period-1);--i){
+    		total += input[i].doubleValue();
     	}
-    	return total/period;
+		return  total/period;
     }
+    
 }
