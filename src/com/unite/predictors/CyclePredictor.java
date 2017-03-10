2033010b883f56e086f13f2dc1a6daf157c23dab
Michael <michael.loukeris@icloud.com>
2017-03-10 13:03:54 +0200

    Adding negative Exception to CyclePredictor

--- src/com/unite/predictors/CyclePredictor.java
+++ src/com/unite/predictors/CyclePredictor.java
@@ -13,7 +13,7 @@ public class CyclePredictor extends AbstractPredictor{
 	 * throws IllegalArgumentException
 	 */
 	public double getResult() {
-		if (size() - period >= 0)
+		if (size() - period >= 0 && period>=0)
 			return data[size() - period];
 		else
 			throw new IllegalArgumentException();
